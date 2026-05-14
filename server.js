1eed6cf7dda5a7af670cbc0b646fd9918800e88f
G Abhinav <g.abhinav174@gmail.com>
2026-05-14 22:49:20 +0530

    Fix router definition

--- server.js
+++ server.js
@@ -2,23 +2,21 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
-import chatRoutes from "./routes/chat.js";
-import speechRoutes from "./routes/speech.js";
-import ttsRoutes from "./routes/tts.js";
 import twilioRoutes from "./routes/twilio.js";
-
 
 dotenv.config();
 
 const app = express();
 
 app.use(cors());
+
 app.use(express.json());
+
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", twilioRoutes);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
